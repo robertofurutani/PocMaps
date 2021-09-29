ef6d6ddc25615e011bcb4e63264c2374ffb9a94f
Roberto Jundi Furutani <roberto.furutani@engdb.com.br>
2021-09-29 14:30:17 -0300

    Remove Key

--- PoCMaps/src/main/java/br/com/engdb/poc/MainDirections.java
+++ PoCMaps/src/main/java/br/com/engdb/poc/MainDirections.java
@@ -3,6 +3,7 @@ package br.com.engdb.poc;
 import java.io.IOException;
 
 import com.google.maps.DirectionsApi;
+import com.google.maps.DirectionsApiRequest.Waypoint;
 import com.google.maps.GeoApiContext;
 import com.google.maps.errors.ApiException;
 import com.google.maps.model.DirectionsLeg;
@@ -14,38 +15,56 @@ import com.google.maps.model.TransitRoutingPreference;
 import com.google.maps.model.TravelMode;
 import com.google.maps.model.Unit;
 
+/*
+ * API Doc:  https://developers.google.com/maps/documentation/directions/get-directions
+ * Lib Java doc: https://github.com/googlemaps/google-maps-services-java/
+ */
 public class MainDirections {
+   
+	private static final String ENDERECO_FIXO = "Av Paulista, 100, São Paulo, SP";
 
 	public static void main(String[] args) {
-		GeoApiContext context = new GeoApiContext.Builder().apiKey("AIzaSyC3bUPf9d79UqE1OCVq5WvH10prLFD8-c4").build();
+		GeoApiContext context = new GeoApiContext.Builder()
+												 .apiKey("AIzaSyC3bUPf9d79UqE1OCVq5WvH1.......")
+												 .build();
 
 		try {
-
+			Waypoint p1 = new Waypoint("Av Paulista, 2150, São Paulo, SP");
+			Waypoint p2 = new Waypoint("Av Pacaembu, 150, São Paulo, SP");
+			Waypoint p3 = new Waypoint("Av Consolação, 522, São Paulo, SP");
+			Waypoint p4 = new Waypoint("Av Sumare, 810, São Paulo, SP");
 			
 			DirectionsResult result = DirectionsApi.newRequest(context)
 					  .mode(TravelMode.DRIVING)
 					  .units(Unit.METRIC)
 					  .region("br")
-		              .origin("Av Paulista, 2150, São Paulo, SP")
-		              .destination( "Av Pacaembu, 150, São Paulo, SP")
+		              .origin(ENDERECO_FIXO)
+		              .waypoints(p1,p2, p3, p4)
+		              .optimizeWaypoints(true)
+		              .destination( ENDERECO_FIXO)
 		              .departureTimeNow()
-		              .alternatives(true)
-		              .trafficModel(TrafficModel.PESSIMISTIC)
-		              .transitRoutingPreference(TransitRoutingPreference.FEWER_TRANSFERS)
+		              .alternatives(false)
+		              .trafficModel(TrafficModel.BEST_GUESS)		              
 		              .language("pt-BR")
 		              .await();
 			
 			DirectionsRoute[] routes = result.routes;
+			long totalTime = 0;
 			for(DirectionsRoute r : routes) {
 				for(DirectionsLeg leg: r.legs) {
 				System.out.println(leg.startAddress + " -- " + leg.endAddress);
+				System.out.println(leg.startLocation + " -- " + leg.endLocation);
 				System.out.println(leg.distance);
 				System.out.println(leg.duration);
-				for (DirectionsStep step : leg.steps) {
-					System.out.println(step);
+				totalTime += leg.duration.inSeconds;
+//				for (DirectionsStep step : leg.steps) {
+//					//System.out.println(step);
+//				}
 				}
-				}
+				
 			}
+			
+			System.out.println("Tempo Total: " + (totalTime/ 60));
 			
 		} catch (ApiException e) {
 			e.printStackTrace();
